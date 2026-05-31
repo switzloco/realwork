91ba563b0ae53537360bf9de99d459ca8b547f24
Switzer, Nick <Nick.Switzer+stryker@stryker.com>
2026-05-30 20:05:00 -0700

    Clean up scratch script

--- list_indexes.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = 'https://USY9HV4C4P-dsn.algolia.net/1/indexes'
-headers = {'X-Algolia-Application-Id': 'USY9HV4C4P', 'X-Algolia-API-Key': 'a66f1d6fe5ef613ce81b6cf0138b4c16'}
-try:
-    print(requests.get(url, headers=headers).json())
-except Exception as e:
-    print(e)
